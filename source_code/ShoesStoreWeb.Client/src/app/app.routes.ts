b6ecbeb586fdd6aebfbd68566fe6c59fe14b8d42
nglong145 <nguyenlong5214@gmail.com>
2025-01-09 15:10:17 +0700

    relsove conflict

--- source_code/ShoesStoreWeb.Client/src/app/app.routes.ts
+++ source_code/ShoesStoreWeb.Client/src/app/app.routes.ts
@@ -2,15 +2,4 @@ import { Routes } from '@angular/router';
 import { ShoppingCartComponent } from './User/Features/cart/shopping-cart/shopping-cart.component';
 import { PaymentComponent } from './User/Features/payment/payment/payment.component';
 
-export const routes: Routes = [
-    // Shopping cart
-    {
-      path: 'cart',
-      component: ShoppingCartComponent,
-    },
-    // payment
-    {
-      path: 'payment',
-      component: PaymentComponent,
-    },
-];
+export const routes: Routes = [];
