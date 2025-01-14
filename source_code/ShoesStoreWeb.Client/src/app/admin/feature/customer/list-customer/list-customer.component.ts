c7786659595d6efd5e24a18c0e22ca189b0f7848
hoangvanhai02 <hahuy9810@gmail.com>
2025-01-15 00:26:34 +0700

    Integrated API Blog into UI

--- source_code/ShoesStoreWeb.Client/src/app/admin/feature/customer/list-customer/list-customer.component.ts
+++ source_code/ShoesStoreWeb.Client/src/app/admin/feature/customer/list-customer/list-customer.component.ts
@@ -11,10 +11,8 @@ export class ListCustomerComponent {
     const selectedValue = (event.target as HTMLSelectElement).value;
     if (selectedValue === 'Block') {
       console.log('User selected Block');
-      // Thực hiện các hành động khi chọn "Block"
     } else if (selectedValue === 'Active') {
       console.log('User selected Active');
-      // Thực hiện các hành động khi chọn "Active"
     }
     
   }
--- source_code/ShoesStoreWeb.Client/src/app/admin/feature/product/addproduct/addproduct.component.ts
+++ source_code/ShoesStoreWeb.Client/src/app/admin/feature/product/addproduct/addproduct.component.ts
@@ -1,12 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Product } from '../models/product.model';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-addproduct',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './addproduct.component.html',
   styleUrl: './addproduct.component.css'
 })
 export class AddproductComponent {
-  //fgjfkgf
+  model: Product;
+  // brand$?: BrandService
+  addProductSub?: Subscription;
+  baseUrl: string = "https://localhost:7158/Images/Product/";
+
+
+  constructor(private router: Router, private productService: ProductService){
+    this.model = {
+      productId: '',
+      productName:'',
+      productImage:'',
+      price: 0,
+      description:'',
+      status:'',
+      brandId: ''
+    }
+  }
+
+  onClick(event : Event):void{
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    if (selectedValue === 'Block') {
+      console.log('User selected Block');
+    } else if (selectedValue === 'Active') {
+      console.log('User selected Active');
+    }
+    
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      this.model.productImage = this.baseUrl + input.files[0].name; // Lấy tên file
+    } else {
+      this.model.productImage = ''; // Không có file được chọn
+    }
+  }
+  onFormSubmit(){
+    console.log(this.model);
+    
+  }
 }
