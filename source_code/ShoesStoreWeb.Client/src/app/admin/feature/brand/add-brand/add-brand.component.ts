85602f30f9aec512edfb7aff6e2795fb5ac2f348
hoangvanhai02 <hahuy9810@gmail.com>
2025-01-15 16:32:27 +0700

    Update forder feature

--- source_code/ShoesStoreWeb.Client/src/app/admin/feature/brand/add-brand/add-brand.component.ts
+++ source_code/ShoesStoreWeb.Client/src/app/admin/feature/brand/add-brand/add-brand.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrandViewModel } from '../model/brand_viewmodel';
+import { Subscription } from 'rxjs';
+import { BrandService } from '../service/brand-service.service';
 
 @Component({
   selector: 'app-add-brand',
@@ -8,5 +11,50 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './add-brand.component.css'
 })
 export class AddBrandComponent {
+  brand: BrandViewModel
+  addBrandSubscription?: Subscription
+  fileImage: File | null = null
+  baseUrl: string = "https://localhost:7158/Images/Product/";
+  constructor(private brandService: BrandService){
+    this.brand = {
+      brandName: '',
+      brandImage: '',
+      description: ''
+    }
+  }
+  ngOnDestroy() {
+    this.addBrandSubscription?.unsubscribe
+  }
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
 
+    if (input.files && input.files.length > 0) {
+      this.brand.brandImage = this.baseUrl + input.files[0].name; // Lấy tên file
+      this.fileImage = input.files[0];
+    } else {
+      this.brand.brandImage = ''; // Không có file được chọn
+    }
+  }
+  addBrand() {
+    if(this.fileImage != null){
+      const formData = new FormData();
+      formData.append('file', this.fileImage)
+      this.addBrandSubscription = this.brandService.uploadImage(formData).subscribe({
+        next: reponse => {
+          this.addBrandSubscription = this.brandService.addBrand(this.brand).subscribe({
+            next: reponse => {
+              console.log("add success")
+            },
+            error: err => {
+              console.log(this.brand)
+              console.log("error")
+            }
+          })
+        },
+        error: err => {
+          console.log("upload file error")
+        }
+      })
+    }
+  }
 }
